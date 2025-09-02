e99d56ee4ad65431fcb0d3e0e1d1db059e573813
Matej Volkmer <volkmmat@fit.cvut.cz>
2025-09-02 18:57:04 +0200

    app init

--- src/main/java/no/hvl/dat250/pollapp/PollAppApplication.java
+++ src/main/java/no/hvl/dat250/pollapp/PollAppApplication.java
@@ -7,31 +7,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// Use with:
-//   http://localhost:8080/hello?name=Matej
-//   http://localhost:8080/goodbye?name=Matej
-//   curl -X POST "http://localhost:8080/greet?name=Matej"
-
 @SpringBootApplication
 @RestController
 public class PollAppApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        SpringApplication.run(PollAppApplication.class, args);
+
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Hello, " + name + "!\n";
-    }
-
-    @GetMapping("/goodbye")
-    public String goodbye(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Goodbye, " + name + "!\n";
-    }
-
-    @PostMapping("/greet")
-    public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Greetings, " + name + "!\n";
-    }
 }
