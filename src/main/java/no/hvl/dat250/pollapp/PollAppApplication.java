d531afe8b5a3c5474c680d8f4287c77b22d0e24f
Matej Volkmer <volkmmat@fit.cvut.cz>
2025-09-06 22:52:33 +0200

    add actions automatic tests

--- src/main/java/no/hvl/dat250/pollapp/PollAppApplication.java
+++ src/main/java/no/hvl/dat250/pollapp/PollAppApplication.java
@@ -2,6 +2,7 @@ package no.hvl.dat250.pollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PollAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(PollAppApplication.class, args);
-
         System.out.println("PollAppApplication started ...");
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Welcome to PollApp</h1>";
     }
 }
