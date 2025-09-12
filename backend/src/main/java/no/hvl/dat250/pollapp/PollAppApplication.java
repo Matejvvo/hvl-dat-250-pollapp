3e3d0bec6fb4183dd2df93c08777fabb6ea83273
Matej Volkmer <volkmmat@fit.cvut.cz>
2025-09-12 15:10:19 +0200

    report 3 done

--- backend/src/main/java/no/hvl/dat250/pollapp/PollAppApplication.java
+++ backend/src/main/java/no/hvl/dat250/pollapp/PollAppApplication.java
@@ -1,15 +1,63 @@
 package no.hvl.dat250.pollapp;
 
+import no.hvl.dat250.pollapp.domain.*;
+import no.hvl.dat250.pollapp.service.*;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.UUID;
 
 @SpringBootApplication
 @RestController
 public class PollAppApplication {
+    private static ConfigurableApplicationContext context;
+    private final PollService p;
+    private final UserService u;
+    private final VoteService v;
+
+    public PollAppApplication(PollService p, UserService u, VoteService v) {
+        this.p = p;
+        this.u = u;
+        this.v = v;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(PollAppApplication.class, args);
+        context = SpringApplication.run(PollAppApplication.class, args);
         System.out.println("PollAppApplication started (front end + back end)...");
+    }
+
+    @GetMapping("/shutdown")
+    public String shutdown() {
+        int exitCode = SpringApplication.exit(context, () -> 0);
+        System.exit(exitCode);
+        return "Application is shutting down...";
+    }
+
+    @GetMapping("/init")
+    public String init() {
+        if (u.list().isEmpty() && p.list().isEmpty() && v.list().isEmpty()) {
+            UUID aliceId = u.create("Alice", "alice@me.com", "").getId();
+            UUID bobId = u.create("Bob", "bob@me.com", "").getId();
+            UUID charlieId = u.create("Charlie", "charlie@me.com", "").getId();
+
+            UUID pollPizzaId = p.create("Ananas on pizza?", 1, false, aliceId, Instant.now(), Instant.now().plus(20, ChronoUnit.DAYS), Arrays.asList("yes", "no", "what?!")).getId();
+            UUID pollTestId = p.create("Test poll?", 1, false, bobId, Instant.now(), Instant.now().plus(20, ChronoUnit.DAYS), Arrays.asList("1", "2", "asdf", "3")).getId();
+
+            p.castVote(aliceId, pollPizzaId, p.listVoteOptions(pollPizzaId).get(0).getId());
+            p.castVote(bobId, pollPizzaId, p.listVoteOptions(pollPizzaId).get(2).getId());
+
+            p.castVote(bobId, pollTestId, p.listVoteOptions(pollTestId).get(1).getId());
+            p.castVote(charlieId, pollTestId, p.listVoteOptions(pollTestId).get(1).getId());
+        }
+        return "OK";
     }
 }
--- experiment-3-report.md
+++ experiment-3-report.md
@@ -1,3 +1,17 @@
 # Report
 
-todo
+[GitHub repository](https://github.com/Matejvvo/hvl-dat-250-pollapp/)
+
+[GitHub: frontend](https://github.com/Matejvvo/hvl-dat-250-pollapp/tree/main/frontend) 
+
+[GitHub: backend](https://github.com/Matejvvo/hvl-dat-250-pollapp/tree/main/backend)
+
+For this assignment I had to choose and learn some frontend framework. Based on the recommendation, I chose Svelte with JS.
+
+I started with building the 3 specified components. Later the fetching logic started to be too complex for these components. So I searched the internet and found out I should create a store file for all the logic.
+
+During programming of the logic, I noticed my implementation of serializing the domain complicates the fetching a bit. For example, matching users votes with the votes on the poll. Maybe it would be better to design the domain in another better way, but for the sake a stuck to the original domain.
+
+Crafting the views with Svelte was easy. Since I have no knowledge of CSS, I asked ChatGPT for a simple styling.
+
+For future improvements, I should implement proper error handling both in backend and frontend. More UI testing would be required and I should look once more into the domain design.
