4d83d217bf07c2a95e827f73a5d7f4079aad4b38
Matej Volkmer <volkmmat@fit.cvut.cz>
2025-10-01 12:53:57 +0200

    lab06: done

--- backend/src/main/java/no/hvl/dat250/pollapp/service/rabbit/VoteEventsListener.java
+++ backend/src/main/java/no/hvl/dat250/pollapp/service/rabbit/VoteEventsListener.java
@@ -37,8 +37,6 @@ public class VoteEventsListener {
         String command = commandFromRoutingKey(routingKey);
         switch (command) {
             case "cast"    -> pollService.castVote(voterId, pollId, optionId);
-            case "updated" -> voteService.update(voteId, voterId, optionId);
-            case "removed" -> voteService.delete(voteId);
             default -> System.out.println("Unknown command: " + command);
         }
     }
